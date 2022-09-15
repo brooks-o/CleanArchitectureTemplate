54915c3010175002dfed26ec441cfea9bf87a8e1
machine-human <dhwltn02@gmail.com>
2022-09-15 14:53:27 +0900

    [develop] feat : manifest 화면 참조 모듈 변경 (app > presentation)

---
+++ presentation/src/main/kotlin/com/example/presentation/view/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.presentation.view
+
+import com.example.base.ui.BaseActivity
+import com.example.presentation.R
+import com.example.presentation.databinding.ActivityMainBinding
+
+class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override fun setupBinding() {}
+
+    override fun setupData() {}
+
+    override fun setupClickListener() {}
+
+    override fun setupUI() {}
+
+    override fun setupObserver() {}
+}
