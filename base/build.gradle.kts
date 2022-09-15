04eca07752d9c4a0686d41b8acf1ea8825df3666
machine-human <dhwltn02@gmail.com>
2022-09-15 10:57:16 +0900

    [develop] init : base 모듈 추가

---
+++ base/build.gradle.kts
@@ -0,0 +1,46 @@
+import com.example.buildSrc.Configuration
+import com.example.buildSrc.Libraries
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = Configuration.compileSdk
+
+    defaultConfig {
+        minSdk = Configuration.minSdk
+        targetSdk = Configuration.targetSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(project(":domain"))
+    implementation(Libraries.CORE_KTX)
+    implementation(Libraries.APPCOMPAT)
+    implementation(Libraries.MATERIAL)
+    testImplementation(Libraries.JUNIT)
+    androidTestImplementation(Libraries.TEST_EXT_JUNIT)
+    androidTestImplementation(Libraries.TEST_ESPRESSO)
+}
