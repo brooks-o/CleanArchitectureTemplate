17bfef6d942d2dfbd7a777de7fbb1ae12c4574f2
machine-human <dhwltn02@gmail.com>
2022-09-15 10:51:35 +0900

    [develop] refactor : data / domain / presentation 모듈 buildSrc 적용

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,14 +1,17 @@
+import com.example.buildSrc.Configuration
+import com.example.buildSrc.Libraries
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
-    compileSdk = 32
+    compileSdk = Configuration.compileSdk
 
     defaultConfig {
-        minSdk = 26
-        targetSdk = 32
+        minSdk = Configuration.minSdk
+        targetSdk = Configuration.targetSdk
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -33,11 +36,10 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    implementation(Libraries.CORE_KTX)
+    implementation(Libraries.APPCOMPAT)
+    implementation(Libraries.MATERIAL)
+    testImplementation(Libraries.JUNIT)
+    androidTestImplementation(Libraries.TEST_EXT_JUNIT)
+    androidTestImplementation(Libraries.TEST_ESPRESSO)
 }
