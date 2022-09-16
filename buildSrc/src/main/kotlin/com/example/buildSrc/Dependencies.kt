8bfc87131c0f2d405130f8601437d28161083735
machine-human <dhwltn02@gmail.com>
2022-09-16 16:21:43 +0900

    [develop] feat : 라이브러리 추가 (activity, kotlin - ktx / hilt compiler / rx3 support / okhttp)

--- buildSrc/src/main/kotlin/com/example/buildSrc/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/example/buildSrc/Dependencies.kt
@@ -8,11 +8,19 @@ object Versions {
     const val TEST_EXT_JUNIT_VERSRION = "1.1.3"
     const val TEST_ESPRESSO_CORE_VERSION = "3.4.0"
 
+    const val ACTIVITY_KTX_VERSION = "1.5.1"
+    const val FRAGMENT_KTX_VERSION = "1.5.2"
+
     const val MOCKITO_VERSION = "4.0.0"
+
     const val RXKOTLIN_VERSION = "3.0.1"
+
     const val APOLLO_VERSION = "3.6.0"
-    const val GOOGLE_HILT_VERSION = "2.38.1"
-    const val ANDROID_HILT_VERSION = "1.3.0"
+
+    const val GOOGLE_HILT_VERSION = "2.43.2"
+    const val ANDROID_HILT_VERSION = "1.0.0-alpha03"
+
+    const val OKHTTP_VERSION = "5.0.0-alpha.2"
 }
 
 object Libraries {
@@ -23,11 +31,23 @@ object Libraries {
     const val TEST_EXT_JUNIT = "androidx.test.ext:junit:1.1.3"
     const val TEST_ESPRESSO = "androidx.test.espresso:espresso-core:3.4.0"
 
+    const val ACTIVITY_KTX = "androidx.activity:activity-ktx:${Versions.ACTIVITY_KTX_VERSION}"
+    const val FRAGMENT_KTX = "androidx.fragment:fragment-ktx:${Versions.FRAGMENT_KTX_VERSION}"
+
     const val MOCKITO = "org.mockito.kotlin:mockito-kotlin:${Versions.MOCKITO_VERSION}"
+
     const val RXKOTLIN = "io.reactivex.rxjava3:rxkotlin:${Versions.RXKOTLIN_VERSION}"
+
     const val APOLLO_RUNTIME = "com.apollographql.apollo3:apollo-runtime:${Versions.APOLLO_VERSION}"
+    const val APOLLO_RX3_SUPPORT = "com.apollographql.apollo3:apollo-rx3-support:${Versions.APOLLO_VERSION}"
     const val APOLLO_API = "com.apollographql.apollo3:apollo-api:${Versions.APOLLO_VERSION}"
 
     const val HILT_ANDROID = "com.google.dagger:hilt-android:${Versions.GOOGLE_HILT_VERSION}"
     const val HILT_GOOGLE_COMPILER = "com.google.dagger:hilt-android-compiler:${Versions.GOOGLE_HILT_VERSION}"
+    const val HILT_COMPILER = "androidx.hilt:hilt-compiler:${Versions.ANDROID_HILT_VERSION}"
+    const val HILT_LIFECYCLE_VIEWMODEL = "androidx.hilt:hilt-lifecycle-viewmodel:${Versions.ANDROID_HILT_VERSION}"
+
+    const val LOGGING_INTERCEPTOR = "com.squareup.okhttp3:logging-interceptor:${Versions.OKHTTP_VERSION}"
+    const val OKHTTP = "com.squareup.okhttp3:okhttp:${Versions.OKHTTP_VERSION}"
+
 }
